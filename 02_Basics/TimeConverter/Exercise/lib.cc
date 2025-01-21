06be4d907a901f2877a2333a9662a47705e625a0
CharlesBronson72 <victor@dr-wiech.de>
2025-01-22 00:11:04 +0100

    timeconverter fertig

--- 02_Basics/TimeConverter/Exercise/lib.cc
+++ 02_Basics/TimeConverter/Exercise/lib.cc
@@ -4,24 +4,30 @@
 
 float convert_milliseconds_to_seconds(const float milliseconds)
 {
+    return milliseconds/1000;
 }
 
 float convert_seconds_to_minutes(const float seconds)
 {
+    return seconds/60;
 }
 
 float convert_minutes_to_hours(const float minutes)
 {
+    return minutes/60;
 }
 
 float convert_milliseconds_to_minutes(const float milliseconds)
 {
+    return convert_seconds_to_minutes(convert_milliseconds_to_seconds(milliseconds));
 }
 
 float convert_milliseconds_to_hours(const float milliseconds)
 {
+    return convert_minutes_to_hours(convert_milliseconds_to_minutes(milliseconds));
 }
 
 float convert_seconds_to_hours(const float seconds)
 {
+    return convert_minutes_to_hours(convert_seconds_to_minutes(seconds));
 }
