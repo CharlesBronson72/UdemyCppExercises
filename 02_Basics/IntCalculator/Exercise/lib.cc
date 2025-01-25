c275403ab8c06a3f1ca2314e756aa3462ffa1735
CharlesBronson72 <victor@dr-wiech.de>
2025-01-25 23:08:36 +0100

    finale Version

--- 02_Basics/IntCalculator/Exercise/lib.cc
+++ 02_Basics/IntCalculator/Exercise/lib.cc
@@ -11,38 +11,35 @@ std::uint32_t modulo(const std::uint32_t number_a, const std::uint32_t number_b)
     else return 0;
 }
 
-std::uint32_t sum_of_digits(std::uint32_t number)
+std::uint32_t sum_of_digits(const std::uint32_t number)
 {
-    if (number==0)
+   /* if (number==0)
     {
         return 1;
-    }
+    }*/
 
-    uint32_t digits = 1;
-    uint32_t number_loop = number;
+    std::uint32_t digits = 0;
+    std::uint32_t number_loop = number;
     do
     {
         digits++;
-        number_loop/=10;
-    }while((number_loop/10) != 0);
+        number_loop /= 10;
+    }while(number_loop > 0);
 
     return digits;
 
 }
 
-std::uint32_t cross_sum(std::uint32_t number)
+std::uint32_t cross_sum(const std::uint32_t number)
 {
-    uint32_t cross_sumvar = 0;
-    uint32_t number_loop = number;
+    std::uint32_t cross_sumvar = 0;
+    std::uint32_t number_loop = number;
 
-    do
+    while (number_loop>0)
     {
-        if (number_loop%10 != 0)
-        {
-            cross_sumvar+=number_loop%10;
-        }
+        cross_sumvar+=number_loop%10;
         number_loop/=10;
-    }while ((number_loop/10) != 0 || number_loop%10);
+    }
 
     return cross_sumvar;
 }
