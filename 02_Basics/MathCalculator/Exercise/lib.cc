74e286b7e0c8185cf8c8456c50cd7e8c47289bed
CharlesBronson72 <victor@dr-wiech.de>
2025-01-26 10:39:36 +0100

    ohne zahl.0

--- 02_Basics/MathCalculator/Exercise/lib.cc
+++ 02_Basics/MathCalculator/Exercise/lib.cc
@@ -5,8 +5,31 @@
 
 double calculate_pi(const std::uint32_t num_iterations)
 {
+    double calculate=0;
+
+    for (std::uint32_t n = 0; n <= num_iterations; n++)
+    {
+        calculate += ((1/(4*n+1)) - (1/(4*n+3)));
+    }
+    calculate*=4;
+    return calculate;
 }
 
 void print_dec_to_bin(std::uint8_t value)
 {
+    std::uint32_t binary=0;
+    while(value>0)
+    {
+        if (value%2 == 0)
+        {
+            binary*=10;
+        }
+        else
+        {
+            binary+=1;
+        }
+        value/=2;
+    }
+
+    std::cout << binary;
 }
